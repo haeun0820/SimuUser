f8bbc7276124de9ef056d2b0e594cfb290b21ba8
temp <temp>
2026-04-09 20:42:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/simuuser/SimuuserApplication.java
@@ -0,0 +1,13 @@
+package com.example.simuuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simuuser/SimuuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simuuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
