45581ccdef198a36661018414db9a4a031bcc6e7
(유나)DESKTOP-VVFOHLM\강보원 <wannaturi@gmail.com>
2026-04-13 20:42:25 +0900

    프로젝트 상세보기 버튼 연결

--- src/main/java/com/example/simuuser/controller/TestController.java
+++ src/main/java/com/example/simuuser/controller/TestController.java
@@ -1,12 +1,22 @@
 package com.example.simuuser.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TestController {
     @GetMapping("/dashboard")
     public String dashboard() {
         return "dashboard/dashboard";
+    }
+
+            // {projectId} 부분이 URL의 숫자를 받아주는 변수 역할을 합니다.
+    @GetMapping("/project/detail/{projectId}")
+    public String projectDetail(@PathVariable("projectId") String projectId, Model model) {
+        // 받아온 ID를 타임리프 페이지로 넘겨줍니다.
+        model.addAttribute("projectId", projectId);
+        return "project/exact_project"; // 실제 html 파일 경로
     }
 }
